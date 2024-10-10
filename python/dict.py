ba5ec5ee7122c857e0f9f75394e7b631ab1f9211
mrsharkus12 <rasulkazakov1@gmail.com>
2024-10-11 00:25:44 +0300

    Update dict.py

--- python/dict.py
+++ python/dict.py
@@ -1,21 +1,15 @@
 import json
 
-SampleDict = {
-    "workers": {
-        "john": {
-            "occupation": "director",
-            "wage": 1500
-        },
-    }
-}
-
+SampleDict = {"workers": {"john": {"occupation": "director", "wage": 1500}}}
 DefaultFileName = "Default"
+
 try:
     with open(DefaultFileName, "r", encoding='utf-8') as file:
         string = json.load(file)
         dict = string
         # print(f"Successfully read {DefaultFileName}")
 except FileNotFoundError:
+    # create a sample file
     with open(DefaultFileName, "w") as file:
         string = json.dumps(SampleDict)
         file.write(string)
@@ -27,22 +21,26 @@ except FileNotFoundError:
 
 while True:
     tempDict = dict["workers"]
-    print("Make a selection (edit, view, delete, save, load, end)")
+    print("Make a selection (Edit, View, Delete, Save, Load, End)")
     sel = input()
     sel = sel.lower().replace(" ", "")
 
-    if sel == "edit":
+    if sel == "edit" or sel == "e":
+        editType = ""
         print(tempDict)
         input1 = input("Name: ")
         input2 = input("Parameter: ")
         input3 = input("Value: ")
         if input1 not in tempDict:
             tempDict[input1] = {}
+            editType = "Added"
+        else:
+            editType = "Edited"
         tempDict[input1][input2] = input3
-        print(f"Edited {input1}'s {input2} to {input3}")
-    elif sel == "view":
+        print(f"{editType} {input1}'s {input2} to {input3}")
+    elif sel == "view" or sel == "v":
         print(tempDict)
-    elif sel == "delete":
+    elif sel == "delete" or sel == "d" or sel == "del":
         print(tempDict)
         input1 = input("Name: ")
         try:
@@ -50,22 +48,28 @@ while True:
             print(f"Removed {input1} from current dictionary")
         except KeyError:
             print("Item does not exist")
-    elif sel == "save":
+    elif sel == "save" or sel == "s":
         print('Enter file name')
         fileName = input("")
-        with open(fileName, "w") as file:
-            string = json.dumps(dict)
-            file.write(string)
-        print(f"Successfully saved to {fileName}")
-    elif sel == "load":
+        try:
+            with open(fileName, "w") as file:
+                string = json.dumps(dict)
+                file.write(string)
+            print(f"Successfully saved to {fileName}")
+        except OSError:
+            print("Invalid character used")
+    elif sel == "load" or sel == "l":
         print('Enter file name')
         fileName = input("")
-        with open(fileName, "r", encoding='utf-8') as file:
-            string = json.load(file)
-            dict = string
-            print(f"Successfully read {fileName}")
-            print(dict)
-    elif sel == "end":
+        try:
+            with open(fileName, "r", encoding='utf-8') as file:
+                string = json.load(file)
+                dict = string
+                print(f"Successfully read {fileName}")
+                print(dict)
+        except FileNotFoundError:
+            print("File not found")
+    elif sel == "end" or sel == "exit":
         break
     else:
         print("Wrong selection")
